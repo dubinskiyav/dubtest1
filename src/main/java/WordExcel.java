84c935b5f68ffe82e40aec597c0d8ee4d1d0962f
dav <dav@gelicon.biz>
2021-12-24 20:54:50 +0500

    WordExcel

--- src/main/java/WordExcel.java
+++ src/main/java/WordExcel.java
@@ -7,8 +7,6 @@ import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,8 +18,10 @@ public class WordExcel {
 
     public void test() {
         System.out.println("WordExcel.test");
+        // Относительно resource
         XWPFDocument doc = readFile("Файл.docx");
-        doc = readFile("d:/WORK/Programming/dubtest1/src/main/resources/Файл.docx");
+        // Абсолютное имя
+        // doc = readFile("d:/WORK/Programming/dubtest1/src/main/resources/Файл.docx");
         // Список run-ов документа
         List<XWPFRun> list = getRunList(doc);
         for (int i = 0; i <= list.size() - 1; i++) {
@@ -33,22 +33,22 @@ public class WordExcel {
     public XWPFDocument readFile(String inputFileName) {
         // Ввод из шаблона
         System.out.println("readFile...");
-        InputStream inputStream = null;
-        FileInputStream fileInputStream = null;
+        InputStream inputStream;
+        FileInputStream fileInputStream;
         try {
             // Читаем из ресурса, а не по абсолютному пути
             inputStream = this.getClass().getClassLoader().getResourceAsStream(inputFileName);
             if (inputStream == null) {
                 // Пробуем по абсолютному пути
                 fileInputStream = new FileInputStream(inputFileName);
-                inputStream = (InputStream) fileInputStream;
+                inputStream =fileInputStream;
             }
         } catch (Exception e) {
             throw new RuntimeException(
                     "Ошибка при чтении файла: " + e.getMessage());
         }
         // открываем файл и считываем его содержимое в объект XWPFDocument
-        XWPFDocument docxFile = null;
+        XWPFDocument docxFile;
         try {
             docxFile = new XWPFDocument(OPCPackage.open(inputStream));
         } catch (Exception e) {
@@ -67,10 +67,8 @@ public class WordExcel {
         // Цикл по параграфам документа
         for (XWPFParagraph par : doc.getParagraphs()) {
             // Цикл по run-ам параграфа. Run - единица текста
-            for (XWPFRun run : par.getRuns()) {
-                // Добавим в список
-                list.add(run);
-            }
+            // Добавим в список все run-ы параграфа
+            list.addAll(par.getRuns());
         }
         // Цикл по таблицам документа
         for (XWPFTable tbl : doc.getTables()) {
@@ -80,11 +78,8 @@ public class WordExcel {
                 for (XWPFTableCell cell : row.getTableCells()) {
                     // По параграфам в ячейке
                     for (XWPFParagraph p : cell.getParagraphs()) {
-                        // По run-ам
-                        for (XWPFRun run : p.getRuns()) {
-                            // Добавим в список
-                            list.add(run);
-                        }
+                        // Добавим в список все run-ы параграфа
+                        list.addAll(p.getRuns());
                     }
                     // цикл по таблицам ячейки
                     for (XWPFTable tbl1 : cell.getTables()) {
@@ -94,11 +89,8 @@ public class WordExcel {
                             for (XWPFTableCell cell1 : row1.getTableCells()) {
                                 // По параграфам в ячейке
                                 for (XWPFParagraph p1 : cell1.getParagraphs()) {
-                                    // По run-ам
-                                    for (XWPFRun run1 : p1.getRuns()) {
-                                        // Добавим в список
-                                        list.add(run1);
-                                    }
+                                    // Добавим в список все run-ы параграфа
+                                    list.addAll(p1.getRuns());
                                 }
                             }
                         }
